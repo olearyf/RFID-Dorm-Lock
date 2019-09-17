36975d31ec4bec2608bdc04882adeecfa320e646
Frances O'Leary <fraoleary@gmail.com>
2019-09-17 11:35:56 -0400

    Update servo_test.ino

--- servo_test.ino
+++ servo_test.ino
@@ -1,10 +1,10 @@
-#include <servo.h>  //add '<' and '>' before and after servo.h
+#include <servo.h>
  
 int servoPin = 9;
  
 Servo servo;  
  
-int servoAngle = 0;   // servo position in degrees
+int servoAngle = 0;
  
 void setup()
 {
@@ -15,33 +15,26 @@ void setup()
  
 void loop()
 {
-//control the servo's direction and the position of the motor
+
  
-   servo.write(45);      // Turn SG90 servo Left to 45 degrees
-   delay(1000);          // Wait 1 second
-   servo.write(90);      // Turn SG90 servo back to 90 degrees (center position)
-   delay(1000);          // Wait 1 second
-   servo.write(135);     // Turn SG90 servo Right to 135 degrees
-   delay(1000);          // Wait 1 second
-   servo.write(90);      // Turn SG90 servo back to 90 degrees (center position)
+   servo.write(45);    
+   delay(1000);         
+   servo.write(90);      
+   delay(1000);         
+   servo.write(135);     
+   delay(1000);          
+   servo.write(90);      
    delay(1000);
 
-//end control the servo's direction and the position of the motor
- 
- 
-//control the servo's speed  
- 
-//if you change the delay value (from example change 50 to 10), the speed of the servo changes
-  for(servoAngle = 0; servoAngle < 180; servoAngle++)  //move the micro servo from 0 degrees to 180 degrees
+  for(servoAngle = 0; servoAngle < 180; servoAngle++)
   {                                  
     servo.write(servoAngle);              
     delay(50);                  
   }
  
-  for(servoAngle = 180; servoAngle > 0; servoAngle--)  //now move back the micro servo from 0 degrees to 180 degrees
+  for(servoAngle = 180; servoAngle > 0; servoAngle--)
   {                                
     servo.write(servoAngle);          
     delay(10);      
   }
-  //end control the servo's speed  
 }
